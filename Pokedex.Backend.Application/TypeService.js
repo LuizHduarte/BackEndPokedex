8729d3bd430ac1df91b51fb22d2ed39e3d5d2954
LuizHduarte <luiz.duarte100@hotmail.com>
2022-12-04 23:18:45 -0300

    Added AddType endpoint

---
+++ Pokedex.Backend.Application/TypeService.js
@@ -0,0 +1,25 @@
+import { AddTypeRepository, TypeOneSearchRepository } from "../Pokedex.Backend.Infra.Data/TypeRepository.js";
+
+export async function AddTypeService(type){
+    const typeOneSearchRepository = await TypeOneSearchRepository(type);
+
+    if(Object.keys(typeOneSearchRepository).length > 0){
+        return{
+            status:400,
+            data:"Impossível inserir um tipo já existente"
+        }
+    }
+    
+    const result = await AddTypeRepository(type);
+    if(result === 1){
+         return {
+            status : 200,
+            data:"Tipo adicionado com sucesso",
+        };
+    }
+    
+    return{
+        status:400,
+        data: "Não foi possível inserir o tipo, por favor tente novamente."
+    };
+}
