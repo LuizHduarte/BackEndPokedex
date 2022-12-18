86056dc0adb1b3d6f7ebb828f9e0c1f5c1b24893
LuizHduarte <luiz.duarte100@hotmail.com>
2022-12-18 16:03:46 -0300

    Added default route

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import "pg";
 import cors from "cors";
 import bodyparser from "body-parser";
 import {port} from "./config.js";
@@ -13,6 +12,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(bodyparser.json());
+
+app.get("/", async (req, res) => {
+  res.status(200).send("ok");
+});
 
 app.get("/pokemon", async (req, res) => {
   const response = await GetPokemonsService();
